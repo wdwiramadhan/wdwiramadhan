ef0ba69e6a6586940fd1a7e05322d84d70d25600
wdwiramadhan <wahyu_dr71@yahoo.com>
2020-05-29 00:16:32 +0700

    update margin profile

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container mx-auto flex">
     <div class="flex flex-wrap w-full lg:w-2/3 mx-auto md:my-20 sm:my-20 lg:my-20 my-10 px-6 py-4">
-      <div class="w-full font-bold text-6xl text-gray-900 mb-4">Hello.</div>
-      <div class="w-full sm:w-1/4 md:w-1/5 lg:w-1/5 mr-5 mb-4">
+      <div class="w-full font-bold text-6xl text-gray-900 mb-6">Hello.</div>
+      <div class="w-full sm:w-1/4 md:w-1/5 lg:w-1/5 mr-5 mb-6">
         <img class="rounded-full" src="../public/profil.jpg">
       </div>
       <div class="w-full sm:w-3/5 md:w-3/5 lg:w-3/5 mt-3">
@@ -10,7 +10,7 @@
         <div class="font-bold text-gray-800 text-3xl">Web Developer.</div>
         <div class="font-bold text-gray-800 text-3xl">Semarang, Indonesia.</div>
       </div>
-      <div class="w-full font-bold text-gray-800 text-3xl mt-12">
+      <div class="w-full font-bold text-gray-800 text-3xl mt-10">
         <a href="https://github.com/wdwiramadhan" target="_blank">Github.</a>
         <a href="https://twitter.com/wdwiramadhan" target="_blank">Twitter.</a>
         <a href="https://www.instagram.com/wahyu2ramadhan/" target="_blank">Instagram.</a>

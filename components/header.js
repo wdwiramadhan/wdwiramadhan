c1da76869951748ecc8522f5252bbc20f9ac0b4b
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-14 15:48:00 +0700

    remove linkedin socmed

--- components/header.js
+++ components/header.js
@@ -7,7 +7,7 @@ export default function Header() {
     <section className="md:flex-row flex justify-between mb-8 px-5 md:px-20 lg:px-48 py-3 border-b">
         <Link href="/">
           <a className="text-xl sm:text-2xl  font-bold text-gray-800 hover:text-gray-700 my-auto">
-            wdwiramadhan.
+            wdwiramadhan
           </a>
         </Link>
         <Link href="/posts">

700532ab4c2d8c628fea7d826ad8c5e5222db14e
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-07-29 06:24:57 +0700

    change desc

--- pages/index.js
+++ pages/index.js
@@ -13,9 +13,9 @@ export default function Index() {
         </Head>
         <Header />
         <Container>
-          <div className="flex flex-col mx-auto max-w-xl">
+          <div className="flex flex-col mx-auto max-w-2xl">
             <p className="text-4xl font-bold text-gray-800 sm:mt-10 mt-3">Hello</p> 
-            <p className="text-lg leading-relaxed mb-4 text-gray-800 mt-3">I'm full-stack developer who uses React or Vue at front-end and Express or Laravel at back-end. I joined Dinus Open Source Community, i'm Open Source enthusiast. Now i'm a freelancer that focus on building websites or help people to solve their problem.</p>
+            <p className="text-lg leading-relaxed mb-4 text-gray-800 mt-3">I'm full-stack developer who uses React or Vue at front-end and Express or Laravel at back-end. I joined Dinus Open Source Community, i'm Open Source enthusiast. Currently i'm a freelancer that focus on building websites.</p>
             <p className="text-lg leading-relaxed mb-4 text-gray-800 mt-3">If you have an exciting project or idea in mind, feel free to contact me</p>
             <Socmed/>
           </div>

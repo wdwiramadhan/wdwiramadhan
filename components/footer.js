3f13e08a1e31cde22605eed995b7534bbfa4c9a8
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-08-25 18:44:32 +0700

    Update footer content

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,27 @@
-import Container from './container'
+import Container from "./container";
 
 export default function Footer() {
+  const yearNow = new Date().getFullYear();
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="mt-24 mb-3">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-28 flex flex-col text-center">
+          <p className="text-sm text-gray-700">
+            ©{yearNow} - Built with
+            <span>
+              <a
+                href="https://nextjs.org/"
+                className="ml-1 text-gray-700 hover:text-blue-500"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="next link"
+              >
+                Next.js
+              </a>
+            </span>
+          </p>
         </div>
       </Container>
     </footer>
-  )
+  );
 }

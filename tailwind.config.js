57e436fe86f837a69d966b961cda8293f04701ea
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-08-25 18:45:22 +0700

    Add max width for profile picture

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
  module.exports = {
   purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'avatar': '50px',
+      }
+    },
   },
   variants: {},
   plugins: [],

08aeb2da77f38aaebda14047d935ebbbd09a86e5
wdwiramadhan <wahyudwiramadhan@gmail.com>
2020-09-06 12:51:15 +0700

    Change space card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import Container from '../components/container'
-import Layout from '../components/layout'
-import Head from 'next/head'
-
+import Container from "../components/container";
+import Layout from "../components/layout";
+import Head from "next/head";
 
 export default function Index() {
   return (
@@ -13,33 +12,44 @@ export default function Index() {
         <Container>
           <section className="flex flex-col mx-auto max-w-2xl mt-12">
             <div className="flex flex-row">
-              <img src="./assets/profile.jpg" alt="profile picture" className="rounded-full max-w-avatar" />
-              <div className="text-xl text-gray-700 px-5 my-auto">wdwiramadhan</div>  
+              <img
+                src="./assets/profile.jpg"
+                alt="profile picture"
+                className="rounded-full max-w-avatar"
+              />
+              <div className="text-xl text-gray-700 px-5 my-auto">
+                wdwiramadhan
+              </div>
             </div>
             <div className="rounded-md overflow-hidden shadow-md md:px-6 md:py-8 px-4 py-6 mt-6">
-              <p className="text-2xl font-bold text-gray-700">Wahyu Dwi Ramadhan</p> 
-              <p className="text-md leading-relaxed text-gray-700 mt-6">I'm full-stack developer who uses React or Vue at front-end and Express or Laravel at back-end. I'm Open Source enthusiast. Currently i'm a freelancer that focus on building websites.</p>
-              <p className="text-md leading-relaxed text-gray-700 mt-8">If you have an exciting project or idea in mind, feel free to contact me</p>
+              <p className="text-2xl font-bold text-gray-700">
+                Wahyu Dwi Ramadhan
+              </p>
+              <p className="text-md leading-relaxed text-gray-700 mt-6">
+                I'm full-stack developer who uses React or Vue at front-end and
+                Express or Laravel at back-end. I'm Open Source enthusiast.
+                Currently i'm a freelancer that focus on building websites.
+              </p>
+              <p className="text-md leading-relaxed text-gray-700 mt-8">
+                If you have an exciting project or idea in mind, feel free to
+                contact me
+              </p>
             </div>
           </section>
-          <section className="flex flex-col mx-auto max-w-2xl mt-20">
-            <div>
-              <div className="text-lg text-gray-700">Projects</div>  
-            </div>
-            <div className="rounded-md overflow-hidden shadow-md md:px-6 md:py-8 px-4 py-6 mt-6">
-              <p className="text-2xl font-bold text-gray-700">Coming Soon</p>
+          <section className="flex flex-col mx-auto max-w-2xl mt-10 md:mt-16">
+            <div className="rounded-md overflow-hidden shadow-md md:px-6 md:py-8 px-4 py-6">
+              <div className="text-lg text-gray-700">Projects</div>
+              <p className="text-2xl font-bold text-gray-700 mt-3">Coming Soon</p>
             </div>
           </section>
-          <section className="flex flex-col mx-auto max-w-2xl mt-20">
-            <div>
-              <div className="text-lg text-gray-700">Articles</div>  
-            </div>
-            <div className="rounded-md overflow-hidden shadow-md md:px-6 md:py-8 px-4 py-6 mt-6">
-              <p className="text-2xl font-bold text-gray-700">Coming Soon</p>
+          <section className="flex flex-col mx-auto max-w-2xl mt-10 md:mt-16">
+            <div className="rounded-md overflow-hidden shadow-md md:px-6 md:py-8 px-4 py-6">
+              <div className="text-lg text-gray-700">Articles</div>
+              <p className="text-2xl font-bold text-gray-700 mt-3">Coming Soon</p>
             </div>
           </section>
         </Container>
       </Layout>
     </>
-  )
+  );
 }

a9c595d40907e9038bd3438c39335ba59309289d
wdwiramadhan <wahyudwiramadhan@gmail.com>
2025-10-19 12:27:09 +0700

    chore: update biome config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 const App = () => {
   return (
-    <main className="h-screen w-full bg-slate-50 flex">
-      <section className="flex max-w-4xl container mx-auto mt-24 px-4">
-        <div className="flex flex-col w-full">
-          <h1 className="text-3xl font-medium text-slate-700">Wahyu Dwi Ramadhan</h1>
-          <p className="text-lg text-slate-600 font-light mt-2">Software Engineer at Tiket.com</p>
-          <div className="flex gap-2 mt-3">
+    <main className="container mx-auto max-w-4xl px-4">
+      <section className="mt-16">
+        <div className="flex flex-col gap-2 w-full">
+          <h1 className="text-2xl font-medium text-gray-700">Wahyu Dwi Ramadhan</h1>
+          <p className="text-md text-gray-500 font-light">Software Engineer at Tiket.com</p>
+          <div className="flex gap-2">
             <a
               href="https://www.linkedin.com/in/wdwiramadhan"
               target="_self"
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Root element not found");
+  throw new Error('Root element not found');
 }
 
 createRoot(rootElement).render(
